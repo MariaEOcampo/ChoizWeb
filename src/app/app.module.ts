0c8d88695fa211e9704fa3fa5117578cd919f90c
emilia.ocampo <emilia.ocampo@proveedor.ues21.edu.ar>
2021-08-28 23:31:26 -0300

    [SETUP]create projects and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ModalComponent } from './components/modal/modal.component';
+import { ListMedicinesComponent } from './components/list-medicines/list-medicines.component';
+import { FaqComponent } from './components/faq/faq.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    ModalComponent,
+    ListMedicinesComponent,
+    FaqComponent
   ],
   imports: [
     BrowserModule
